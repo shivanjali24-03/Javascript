83af97d55a3942272abc598fdd6129c4d1a36825
shivanjali24-03 <156819273+shivanjali24-03@users.noreply.github.com>
2025-10-07 03:29:43 +0000

    Scope Level

---
+++ 01_Basics/05_scope.js
@@ -0,0 +1,28 @@
+function one()
+{
+        const username = "Shivanjali"
+    function two()
+    {
+        const website = "youtube"
+        console.log(username);
+        
+
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true)
+{
+    const username = "Shivanjali"
+    if(username === "Shivanjali")
+        {
+            const website = "youtube"
+            console.log(username + website);
+            
+        }
+        // console.log(website);
+        
+}
+// console.log(username);
